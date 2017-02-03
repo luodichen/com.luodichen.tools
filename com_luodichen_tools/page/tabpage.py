7912b28a5a89659ada5789c3a1245f88b6720bea
luodichen-mx <dichen.luo@mingxifund.com>
2017-02-03 11:27:17 +0800

    disable 'dns-resolve'

--- com_luodichen_tools/page/tabpage.py
+++ com_luodichen_tools/page/tabpage.py
@@ -16,7 +16,7 @@ class TabPage(object):
 tablist = [
     TabPage(False, '/page/ip/', u'IP 地址', ['js/ip.js', ]),
     TabPage(False, '/page/macinfo/', u'MAC 地址', ['js/macinfo.js', ]),
-    TabPage(False, '/page/dns-resolve/', u'DNS 解析', []), 
+    # TabPage(False, '/page/dns-resolve/', u'DNS 解析', []), 
     TabPage(False, '/page/whois/', 'Whois', ['js/whois.js', ]),
     TabPage(False, '/page/hash/', 'Hash',['js/crypto-js/rollups/md5.js', 
                                           'js/crypto-js/rollups/sha1.js',
